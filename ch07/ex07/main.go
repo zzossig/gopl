8196621eccc48faa6852688f0a0420092bbcf5bf
Choi Sung Hwan <zzossig@gmail.com>
2021-01-01 09:00:00 +0900

    ch07 in progress

--- ch07/ex07/main.go
+++ ch07/ex07/main.go
@@ -5,11 +5,3 @@
 	`Celsius` has a `func (c Celsius) String() string` method, so `Celsius` is a `Stringer`.
 	When `flag` shows the help messages, it will call `Celsius' String` method to format the value.
 */
-
-// What???????????? I just need to know more about the flag package
-
-package main
-
-func main() {
-	
-}
---
+++ ch07/ex08/main.go
@@ -0,0 +1,8 @@
+/*
+	Many GUIs provide a table widget with a stateful multi-tier sort:
+	the primary sort key is the most recently clicked column head,
+	the secondary sort key is the second-most recently clicked column head, and so on.
+	Define an implementation of `sort.Interface` for use by such a table.
+	Compare that approach with repeated sorting using `sort.Stable`.
+*/
+
